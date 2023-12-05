2e77e3d918935545bd982f2a5e0311e7ea637a1c
ItsKingzman <connerk4@gmail.com>
2023-12-05 16:34:45 -0600

    center hire

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -7,15 +7,16 @@ const Footer = ({}) => {
   return (
     <>
       <div className="text-center mt-5 laptop:mt-40 p-2 laptop:p-0">
-        <div>
-          <div className="mt-10">
-            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl font-bold">
-              HIRE ME TODAY
-            </h1>
-            <a href="mailto:connerk4@gmail.com">
-              <Button type="primary">Hire</Button>
-            </a>
-            <div className="mt-10">
+        <div className="flex flex-col items-center"> {/* Flex container for centering */}
+          <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl font-bold">
+            HIRE ME TODAY
+          </h1>
+          <a href="mailto:connerk4@gmail.com" className="mt-5">
+            <Button type="primary">Hire</Button>
+          </a>
+        </div>
+      </div>
+      <div className="mt-10">
             <Socials className="" socials={[
                   {
                     id: "1",
@@ -49,9 +50,6 @@ const Footer = ({}) => {
                   }
               ]} />
             </div>
-          </div>
-        </div>
-      </div>
       <h1 className="text-center text-sm font-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
         <Link href="http://www.KingzmanCo.com">
